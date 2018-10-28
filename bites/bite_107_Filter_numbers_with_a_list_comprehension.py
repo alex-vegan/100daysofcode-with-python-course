f69fd2f42d85efb4a9c65de7a7940ac34f7361fa
alex-vegan <alex-vegan@outlook.com>
2018-10-28 22:52:07 +0500

    delete dublicate of bites from repository

--- bites/bite_107_Filter_numbers_with_a_list_comprehension.py
+++
@@ -1,5 +0,0 @@
-def filter_positive_even_numbers(numbers):
-    """Receives a list of numbers, and filters out numbers that
-       are both positive and even (divisible by 2), try to use a
-       list comprehension"""
-    return [num for num in numbers if num > 0 and num % 2 == 0]
