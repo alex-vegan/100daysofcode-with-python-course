5f97888e8f269c69b89f79e0ede076344f041818
alex-vegan <alex-vegan@outlook.com>
2018-11-10 22:52:01 +0500

    nytimes_scraper

---
+++ days/day048/nytimes_scraper.py
@@ -0,0 +1,31 @@
+import bs4
+import shared_funcs
+
+url = 'https://www.nytimes.com/section/technology/personaltech'
+
+
+def scrape(site):
+    header_list = []
+    soup = bs4.BeautifulSoup(site.text, 'lxml')
+    html_header_list = soup.select('.headline')
+    for html_header in html_header_list:
+        header_list.append(html_header.getText())
+    return list(set(header_list))
+
+
+def main():
+    site = shared_funcs._pull_site(url)
+    headers = scrape(site)
+    with open('itdict.txt') as file_dict:
+        it_dict = file_dict.read().strip().lower().split()
+        #print(it_dict)
+        for header in headers:
+            for header_word in header.lower().split():
+                if header_word in it_dict:
+                    print(header.strip())
+                    break
+    return None
+
+
+if __name__ == '__main__':
+    main()
